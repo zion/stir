06607433a06117d7da0a70378d01738a367404c1
uchagani <umair.chagani@manheim.com>
2016-03-29 17:42:24 -0400

    fix bug where no match is found

--- lib/stir/core_ext/string.rb
+++ lib/stir/core_ext/string.rb
@@ -1,8 +1,9 @@
 module CoreExt
   module String
-    def interpolate!(h)
+    def interpolate(h)
       return self if h.nil?
-      self.gsub!(/%({\w+})/) do
+      self.gsub(/%({\w+})/) do |match|
+        return self if match.nil?
         key = $1.tr('{}', '').to_sym
         raise KeyError.new("key{#{key}} not found") unless h.has_key?(key)
         h[key]
