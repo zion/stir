119ff6102ef99a525a1bba233ffc9f23cf7ce5e1
Wallace Harwood <wallace.harwood@manheim.com>
2016-08-26 08:32:09 -0500

    [wallace] adding methods for code and headers, to match httparty"

--- lib/stir/soap/soap_response.rb
+++ lib/stir/soap/soap_response.rb
@@ -3,10 +3,18 @@ require 'savon/response'
 module Savon
   class Response
 
+    def code
+      self.http.code.to_i
+    end
+
+    def headers
+      self.http.headers
+    end
+
     protected
 
     def method_missing(name, *args, &block)
-        self.body[args.first]
+      self.body[args.first]
     end
 
   end
