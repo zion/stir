bd15fea60f381b88fdf8dcf5d679d71558528b64
Wallace Harwood <wallace.harwood@manheim.com>
2016-09-14 17:53:25 -0500

    refactor verify_version_and_env method
    removing loaderror in exchange for configurationerror, abstracting file verification to its own step

--- lib/stir/base/configuration.rb
+++ lib/stir/base/configuration.rb
@@ -16,6 +16,7 @@ module Stir
       end
 
       def reload_configs!
+        verify_file(@config_file)
         verify_version_and_env(@config_file)
         @service_config = load_configs(@config_file)
         @service_config[:headers] = {} if @service_config[:headers].nil?
@@ -52,8 +53,11 @@ module Stir
         YAML.load(ERB.new(File.read(filename)).result)[Stir.version][Stir.environment].with_indifferent_access
       end
 
+      def verify_file(filename)
+        raise(ConfigurationError, "#{filename} not found.") unless File.exists?(filename)
+      end
+      
       def verify_version_and_env(filename)
-        raise LoadError.new("#{filename} not found.") unless File.exists?(filename)
         file = YAML.load(File.read(filename))
         raise(ConfigurationError, "Version: '#{Stir.version}' is not defined for this client in the config file.") unless file.has_key? Stir.version
         raise(ConfigurationError, "Environment: '#{Stir.environment}' is not defined for Version: '#{Stir.version}' in the config file.") unless file[Stir.version].has_key? Stir.environment
