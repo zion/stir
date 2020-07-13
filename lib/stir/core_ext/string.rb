739f7d536195db4fad5c4ce909f76b2ddfbba93f
Simeon <simeonberns@gmail.com>
2020-07-13 10:20:16 -0500

    Removed URI.escape from HTTParty request, Added CGI::escape for each URL param

--- lib/stir/core_ext/string.rb
+++ lib/stir/core_ext/string.rb
@@ -6,7 +6,7 @@ module CoreExt
         return self if match.nil?
         key = $1.tr('{}', '').to_sym
         raise KeyError.new("key{#{key}} not found") unless h.has_key?(key)
-        h[key]
+        CGI::escape(h[key])
       end
     end
   end
